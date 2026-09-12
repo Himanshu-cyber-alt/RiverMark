040171f62e6c1040e3d78bb9da045f3a94aca976
Himanshu Pagare <155243085+Himanshu-cyber-alt@users.noreply.github.com>
2026-09-12 19:54:16 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.get("/",(req,res)=>{
+
+       res.status(200).json({
+        status: "Backend Is Running"
+    });
+})
+
 app.get("/health", (req, res) => {
     res.status(200).json({
         status: "healthy"
