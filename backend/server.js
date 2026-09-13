741a67882c636899445b2bf1ea5ff52dfa44112f
Himanshu Pagare <155243085+Himanshu-cyber-alt@users.noreply.github.com>
2026-09-13 14:04:40 +0530

    test v2

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.get("/",(req,res)=>{
     
        res.status(200).json({
-        status: "Backend Is Running 2026"
+        status: "Hello from Rivermark backend v2"
     });
 })
 
