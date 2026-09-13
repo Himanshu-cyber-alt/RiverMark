14d3a571df69a9e0416408d85ce46c47495142e6
Himanshu Pagare <155243085+Himanshu-cyber-alt@users.noreply.github.com>
2026-09-13 19:24:29 +0530

    fail

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,9 @@ app.get("/api/hello", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 
+throw new Error("fail");
+
+
 app.listen(PORT, () => {
     console.log(`Backend running on port ${PORT}`);
 });
